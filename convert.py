ab6980c491cdc105125af9b3e30dd46a0e0d7884
Li Zhuoying <95523816+joyli-x@users.noreply.github.com>
2024-01-04 22:06:12 +0800

    Delete convert.py

--- convert.py
+++
@@ -1,7 +0,0 @@
-from riscv_assembler.convert import AssemblyConverter as AC
-# instantiate object, by default outputs to a file in nibbles, not in hexademicals
-convert = AC(output_mode = 'f', nibble_mode = False, hex_mode = True)
-
-# Convert a whole .s file to text file
-convert("test_code.s", "test_code.hex")
-
